0dd0660e32c7db323b923a1c3ef166aee02820da
Abdelrhman Kotb <87202131+abdelrhmaKotb@users.noreply.github.com>
2023-06-24 18:45:03 +0300

    ..

--- src/app/company/company-attendance/company-attendance.component.ts
+++ src/app/company/company-attendance/company-attendance.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./company-attendance.component.scss']
 })
 export class CompanyAttendanceComponent implements OnInit {
+  src = 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf';
 
   constructor() { }
 
